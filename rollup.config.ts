cb26701926148ddea81737bdb94b9f3b90c1775f
Anand Chowdhary <mail@anandchowdhary.com>
2020-10-26 16:10:06 +0530

    Remove JS prefix from file name

--- rollup.config.ts
+++ rollup.config.ts
@@ -10,7 +10,7 @@ const pkg = require("./package.json");
 const libraryName = "object-literal-parse";
 
 export default {
-  input: "js-object-literal-parse.ts",
+  input: "object-literal-parse.ts",
   output: [
     {
       file: pkg.main,
@@ -22,7 +22,7 @@ export default {
   ],
   external: [],
   watch: {
-    include: "js-object-literal-parse.ts",
+    include: "object-literal-parse.ts",
   },
   plugins: [
     json(),
